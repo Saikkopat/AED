2d8a08977bfe6527791169835f355990af664816
Ángel Cárdenas <saikkopat@protonmail.com>
2022-11-12 11:52:33 -0600

    Added ADTs

--- 1P/list2/Pr2-E1.c
+++ 1P/list2/Pr2-E1.c
@@ -6,35 +6,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+typedef struct {
     int dia, mes, anio;
 } Info_fecha;
 
-typedef struct{
-    char * nombre;
+typedef struct {
+    char *nombre;
     int edad;
-    Info_fecha * fe;
+    Info_fecha *fe;
 } Alumno;
 
-Alumno *crearAlumno(int n){
+Alumno *crearAlumno(int n) {
     Alumno *nuevo;
     int i;
     nuevo = (Alumno *)malloc(n * sizeof(Alumno));
-    if(nuevo == NULL){
+    if (nuevo == NULL) {
         printf("No es posible crear alumnos.\n");
         exit(0);
     }
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
         nuevo[i].nombre = (char *)malloc(40 * sizeof(char));
         nuevo[i].fe = (Info_fecha *)malloc(2 * sizeof(Info_fecha));
     }
     return nuevo;
 }
 
-void ingAlumno(Alumno *inscritos, int n){
+void ingAlumno(Alumno *inscritos, int n) {
     int i;
     printf("Ingresa la información de los alumnos:\n");
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
         printf(" Alumno #%d:\n", i + 1);
         printf("  Nombre: ");
         scanf(" %[^\n]", inscritos[i].nombre);
@@ -55,50 +55,62 @@ void ingAlumno(Alumno *inscritos, int n){
     }
 }
 
-void impAlumnos(Alumno *inscritos, int n){
+void impAlumnos(Alumno *inscritos, int n) {
     int i;
     printf("\n");
     printf("   Lista de Alumnos inscritos:\n");
-    for(i = 0; i < n; i++){
-        printf("  Alumno %d\n   %s\n   %d\n   Fecha:\n    %d  %d\n    %d  %d\n    %d  %d",i+1, inscritos[i].nombre, inscritos[i].edad, inscritos[i].fe[0].dia, inscritos[i].fe[1].dia, inscritos[i].fe[0].mes, inscritos[i].fe[1].mes, inscritos[i].fe[0].anio, inscritos[i].fe[1].anio);
+    for (i = 0; i < n; i++) {
+        printf("  Alumno %d\n   %s\n   %d\n   Fecha:\n    %d  %d\n    %d  %d\n    %d  %d", i + 1, inscritos[i].nombre, inscritos[i].edad, inscritos[i].fe[0].dia, inscritos[i].fe[1].dia, inscritos[i].fe[0].mes, inscritos[i].fe[1].mes, inscritos[i].fe[0].anio, inscritos[i].fe[1].anio);
         printf("\n");
-        }
+    }
 }
 
-void intercambio(Alumno *A, Alumno *B){
-    printf("     %p, %p", A, B);
-    Alumno  temp = *A;
+void intercambio(Alumno *A, Alumno *B) {
+    Alumno temp = *A;
     *A = *B;
     *B = temp;
 }
 
-Alumno * ordenaApe(Alumno *A, int num){
-    Alumno * B;
-	 int i, j, k;
+int findSpace(char *name) {
+    int i = 0;
+    while (name[i] != ' ')
+        i++;
+    return i + 1;
+}
+
+Alumno *ordenaApe(Alumno *A, int num) {
+    Alumno *B;
+    int i, j, apA, apB, flag;
     B = crearAlumno(num);
-	 for (i = 0; i < num; i++){
-		B[i] = A[i];
-	 }
+    for (i = 0; i < num; i++) {
+        B[i] = A[i];
+    }
     printf("\n");
-    for(i = 0; i < num; i++){
-        for(j = 0; j < num - i - 1; j++){
-            for(k = 0; k < strlen(B[j].nombre); k++){
-					if((int)B[j].nombre[k] != (int)B[j + 1].nombre[k]){
-						if((int)B[j].nombre[k] > (int)B[j + 1].nombre[k]){
-						intercambio(&B[j], &B[j + 1]);
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num - i - 1; j++) {
+            apA = findSpace(B[j].nombre);
+            apB = findSpace(B[j + 1].nombre);
+            flag = 1;
+            do {
+                if((int)B[j].nombre[apA] != (int)B[j + 1].nombre[apB]) {
+                    if ((int)B[j].nombre[apA] > (int)B[j + 1].nombre[apB]) {
+                        intercambio(&B[j], &B[j + 1]);
+                    }
+                    flag = 0;
+                } else {
+                    apA++;
+                    apB++;
                 }
-					 k = strlen(B[j].nombre);
-					}
-            }
+            }  while (flag == 1);
         }
     }
     return B;
 }
 
-void liberarMemoria(Alumno *inscritos){
-	printf("   ¡Bloque de memoria %p liberado!\n", inscritos);
-   free(inscritos);
-   printf("\n");
+void liberarMemoria(Alumno *inscritos) {
+    printf("   ¡Bloque de memoria %p liberado!\n", inscritos);
+    free(inscritos);
+    printf("\n");
 }
 
 int main() {
@@ -109,10 +121,10 @@ int main() {
     A = crearAlumno(num);
     ingAlumno(A, num);
     printf("\n");
-    impAlumnos(A, num);
     B = ordenaApe(A, num);
+    impAlumnos(A, num);
     printf("\n");
-	 printf("   Lista de alumnos ordenada alfabeticamente: \n");
+    printf("   Lista de alumnos ordenada alfabeticamente: \n");
     impAlumnos(B, num);
     printf("\n");
     liberarMemoria(A);
---
+++ 2P/TAD Pila/mainPila.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pila.h"
+
+void manejaMsg (int msg) {
+   char* mensajes[4] = {"   \nNo hay memoria disponible.\n",
+                        "   \nMemoria liberada.\n",
+                        "   \nPila llena.\n",
+                        "   \nPila vacia.\n"};
+   printf("%s", mensajes[msg]);
+}
+
+void liberarMemoria (PILA S) {
+	free(S);
+	manejaMsg(1);
+}
+
+void darElemento (PILA S1) {
+   int e, c;
+   do {
+      printf("¿Quieres apilar un nuevo elemento?\n  1.Si\n  2.No\n   ");
+      scanf("%d", &c);
+      if (c == 1) {
+         printf("   Ingresa el elemento a apilar: ");
+         scanf("     %d", &e);
+         apilar(S1, e);
+      }
+   } while (c == 1);
+}
+
+void mostrarPila (PILA S) {
+	PILA copia = crearPila();
+	*copia = *S;
+	if (esPilaVacia(copia) == TRUE) {
+		manejaMsg(3);
+		exit(0);
+	}
+	printf("\n Elementos de la pila:\n");
+	while(esPilaVacia(copia) == FALSE)
+		printf("  %d : %d\n", (copia -> tope) + 1, desapilar(copia)); //Asignacion de valores de derecha a izquierda.
+	liberarMemoria(copia);
+}
+
+PILA invertirPila (PILA S) {
+	PILA copia = crearPila(), temp = crearPila();
+	*copia = *S;
+	liberarMemoria(S);
+	while(esPilaVacia(copia) == FALSE)
+		apilar(temp, desapilar(copia));
+	liberarMemoria(copia);
+	return temp;
+}
+
+int main () {
+   PILA S1;
+   S1 = crearPila();
+   darElemento(S1);
+	mostrarPila(S1);
+   S1 = invertirPila(S1);
+	printf("\n   Pila invertida:\n");
+	mostrarPila(S1);
+   return 0;
+}
